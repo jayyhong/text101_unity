be920bac94e335e7070158a01aebf717e6ebfd27
Jay Hongvarivatana <jayyhong@gmail.com>
2017-08-22 13:15:43 -0700

    Started text101 tutorial unityx

---
+++ Assets/TextController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello world";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			text.text = "You wake up to find yourself in a prison cell, you" +
+						"have to escape before the ward touches your butt again." +
+						"There are som dirty sheets on the bed, a mirror on the wall, "+
+						"and the door is locked from the outside.\n\n" +
+						"Press S to View Sheets \n" +
+						"Press M to View Mirror\n" +
+						"Press L to View Lock";
+		}
+	}
+}
